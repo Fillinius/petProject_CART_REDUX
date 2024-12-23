602ca8772954c33bff200e50dffcf3135b800c10
Vladimir Revyagin <skorabeyf@gmail.com>
2024-12-23 11:43:16 +0300

    init app at  Vite

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1>Basket + React + Vite</h1>
+      <div className="card">
+
+      </div>
+
+    </>
+  )
+}
+
+export default App
