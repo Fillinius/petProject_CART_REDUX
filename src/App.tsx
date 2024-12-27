a7a6e02eef536685b0c3e7470e7578035ced9557
Vladimir Revyagin <skorabeyf@gmail.com>
2024-12-27 16:28:28 +0300

    Добавлен Persistor - хранение состояния в localstorage

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,21 @@ import { StoreCard } from './StoreCard'
 import './App.css'
 import { Cart } from './Cart'
 import { Provider } from 'react-redux'
-import { store } from './redux/store'
+import { persistor, store } from './redux/store'
+import { PersistGate } from 'redux-persist/integration/react'
 
 function App() {
   return (
     <>
-      <div className="container">
-        <Provider store={store}>
-          <NavBar />
-          <StoreCard />
-          <Cart />
-        </Provider>
-      </div>
+      <PersistGate persistor={persistor} loading={<div>Loading...</div>}>
+        <div className="container">
+          <Provider store={store}>
+            <NavBar />
+            <StoreCard />
+            <Cart />
+          </Provider>
+        </div>
+      </PersistGate>
     </>
   )
 }
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -3,15 +3,22 @@ import { productsReducer } from './productsReducer'
 import { modalReducer } from './modalReducer'
 import thunkMiddleware from 'redux-thunk'
 import { logActionMiddleware } from './logActionMiddleware'
+import { persistReducer, persistStore } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
-const rootReducer = combineReducers({
-  products: productsReducer,
-  modal: modalReducer,
-})
+const rootReducer = persistReducer(
+  { key: 'root', storage },
+  combineReducers({
+    products: productsReducer,
+    modal: modalReducer,
+  })
+)
 
 export const store = createStore(
   rootReducer,
   applyMiddleware(logActionMiddleware)
 )
+
+export const persistor = persistStore(store)
 
 export type RootState = ReturnType<typeof rootReducer>
